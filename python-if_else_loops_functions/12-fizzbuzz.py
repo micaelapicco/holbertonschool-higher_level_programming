e97ec3eba19028d1a5c5b4dee9c528bb3521c8e5
micaelapicco <micaelaabril421@gmail.com>
2023-05-30 11:50:33 -0700

    viejo y querido fizz

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,10 +4,7 @@ def fizzbuzz():
         if number % 3 == 0 and number % 5 != 0:
             print("Fizz ", end="")
         elif number % 5 == 0 and number % 3 != 0:
-            if number == 100:
-                print("Buzz", end=" ")
-            else:
-                print("Buzz ", end="")
+            print("Buzz", end=" ")
         elif number % 3 != 0 or number % 5 != 0:
             print(number, end=" ")
         else:
