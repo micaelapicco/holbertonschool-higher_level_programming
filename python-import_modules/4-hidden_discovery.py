dbd18da24a28f39f54404aab93e2b2c799158bb5
micaelapicco <micaelaabril421@gmail.com>
2023-06-01 08:12:54 -0700

    import

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
     for directory in dir(hidden_4):
-        if directory[0] != "__":
+        if directory[0] != "_":
             print(directory)
