b1a475ed762aaab8809e3e5813572e9b4308e0b2
micaelapicco <micaelaabril421@gmail.com>
2023-06-05 11:44:07 -0700

    divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list:
-        copy = []
-        for i in my_list:
-            if i % 2 == 0:
-                copy.append(True)
-            else:
-                copy.append(False)
+    copy = []
+    for i in my_list:
+        if i % 2 == 0:
+            copy.append(True)
+        else:
+            copy.append(False)
     return (copy)
