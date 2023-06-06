98fe0d51bb6327cdc746fc191cf634397df65b83
micaelapicco <micaelaabril421@gmail.com>
2023-06-06 09:20:29 -0700

    number

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    if a_dictionary:
-        result = len(a_dictionary)
-        return (result)
+    result = len(a_dictionary)
+    return (result)
