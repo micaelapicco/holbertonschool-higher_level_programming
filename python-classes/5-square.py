79505520bc01cbd9a3d2c59723e991672defcf35
micaelapicco <micaelaabril421@gmail.com>
2023-06-13 06:38:27 -0700

    square with #

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Class Square,size & area of square"""
+
+
+class Square:
+    """define size of square"""
+
+    def __init__(self, size=0):
+        """if size is not type int or > 0, errors:"""
+        if not type(size) == int:
+            raise TypeError("size must be an integer")
+        if not size >= 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not type(value) == int:
+            raise TypeError("size must be an integer")
+        if not value >= 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Calculate size of square """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
