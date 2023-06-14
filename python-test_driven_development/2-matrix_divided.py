55301a4195c45696b61a406b172106b139108a88
micaelapicco <micaelaabril421@gmail.com>
2023-06-14 11:41:06 -0700

    test 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+matrix divided Module
+'''
+
+
+def matrix_divided(matrix, div):
+    """Errors cases"""
+    if len(set(map(len, matrix))) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+    for array in matrix:
+        for element in array:
+            if type(element) != int and type(element) != float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) != float and type(div) != int:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new = []
+    for element in matrix:
+        new.append(list(map(lambda x: round((x / div), 2), element)))
+    return (new)
