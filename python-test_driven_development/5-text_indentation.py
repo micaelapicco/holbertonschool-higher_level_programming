92595df7a380d2826411c8623b127244446d76bf
micaelapicco <micaelaabril421@gmail.com>
2023-06-15 07:14:29 -0700

    indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,10 @@
 
 
 def text_indentation(text):
-    """prints a text with 2 new lines after each of these characters: ., ? and :"""
+    '''
+    prints a text with 2 new lines after each of these characters:
+    ., ? and :
+    '''
     if type(text) != str:
         raise TypeError("text must be a string")
     flag = 0
