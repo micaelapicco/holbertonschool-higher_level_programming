3dbd7cbc9049033e5276a3db6e029882a523bf52
micaelapicco <micaelaabril421@gmail.com>
2023-06-15 10:04:40 -0700

    rectangle h and w

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Simple rectangle"""
+
+
+class Rectangle:
+    """definition of a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """set errors of width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter of height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """set errors of heigth"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
