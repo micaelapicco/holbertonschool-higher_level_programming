61c78ec6912bb89184390be0aa98c23817bdae32
micaelapicco <micaelaabril421@gmail.com>
2023-06-22 10:05:38 -0700

    json to str

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Task 4:Write a function that returns an object
+(Python data structure) represented by a JSON string:
+"""
+import json
+
+
+def from_json_string(my_str):
+    """from json to string"""
+    python_str = json.loads(my_str)
+    return (python_str)
+
+# 1: json.loads converter json into python object
