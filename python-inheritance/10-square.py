60de890c5a542371e972ec910a8de772f405f933
micaelapicco <micaelaabril421@gmail.com>
2023-06-22 08:15:10 -0700

    square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""inherit from rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Based on Rectangle"""
+
+    def __init__(self, size):
+        """size check"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area of square"""
+        return (self.__size ** 2)
