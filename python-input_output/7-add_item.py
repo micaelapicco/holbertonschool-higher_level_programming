3144f0daefeaeea343b03fa56e720b3a7503e545
micaelapicco <micaelaabril421@gmail.com>
2023-06-22 12:07:58 -0700

    works?

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Task 7: Write a script that adds all arguments to a Python list,
+and then save them to a file
+"""
+import json
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv
+file = "add_item.json"
+with open(file, 'a', encoding="utf-8") as f:
+    my_list = load_from_json_file(file) + args[1:]
+    save_to_json_file(my_list, file)
