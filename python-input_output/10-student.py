5927b7d32be1bc626293415417b554e4f897ecdf
micaelapicco <micaelaabril421@gmail.com>
2023-06-23 07:38:14 -0700

    filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -9,12 +9,15 @@ class Student:
     """student, first and last name, age"""
 
     def __init__(self, first_name, last_name, age):
+        """set first, last name and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """filter json"""
         if attrs is None:
             return self.__dict__
         else:
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return {key: value for key, value in
+                    self.__dict__.items() if key in attrs}
