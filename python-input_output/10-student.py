275d680d3fe3f5f15c952ba3c111ee06d7954d50
micaelapicco <micaelaabril421@gmail.com>
2023-06-23 07:35:52 -0700

    new dict

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task 10: Write a class Student that defines a student by:
+first name, last name and age
+"""
+
+
+class Student:
+    """student, first and last name, age"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
