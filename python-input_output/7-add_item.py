1e2364fc30818843fc725ced650a3b176512e36c
micaelapicco <micaelaabril421@gmail.com>
2023-06-26 07:10:44 -0700

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,10 @@
 Task 7: Write a script that adds all arguments to a Python list,
 and then save them to a file
 """
+
+
 import sys
-import os
+from os import path
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -13,7 +15,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv
 file = "add_item.json"
 
-if os.path.exist(file):
+if path.exists(file):
     my_list = load_from_json_file(file) + args[1:]
 
 else:
