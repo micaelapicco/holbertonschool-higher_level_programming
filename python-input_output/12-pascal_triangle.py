9cedbc44cdfdfdb9ff9a9ba15ad0da7bca8862d3
micaelapicco <micaelaabril421@gmail.com>
2023-06-26 06:52:35 -0700

    new

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""""
+Task 12: Create a function def pascal_triangle(n):
+that returns a list of lists of integers representing
+the Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """Return a last of list of int representing the Pascal Triangle"""
+    triangle = [[1]]
+    for h in range(n - 1):
+        new = []
+        for lst in range(len(triangle[h]) + 1):
+            if lst == 0 or lst == len(triangle[h]):
+                new.append(1)
+            else:
+                new.append(triangle[h][lst - 1] + triangle[h][lst])
+        triangle.append(new)
+    return triangle
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,13 +4,20 @@ Task 7: Write a script that adds all arguments to a Python list,
 and then save them to a file
 """
 import sys
-
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 file = "add_item.json"
-with open(file, 'a', encoding="utf-8") as f:
+#with open(file, 'a', encoding="utf-8") as f:
+if os.path.exist(file):
     my_list = load_from_json_file(file) + args[1:]
-    save_to_json_file(my_list, file)
+    
+else:
+    my_list = []
+    
+save_to_json_file(my_list, file)
+
+# 1: os, library "path"
