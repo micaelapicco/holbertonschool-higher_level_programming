79e361fa21ca88a166245ac87fbb20e348a0f043
micaelapicco <micaelaabril421@gmail.com>
2023-06-26 12:43:20 -0700

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,50 +16,42 @@ class Rectangle(Base):
         self.__X = x
         self.__y = y
 
+    @property
+    def width(self):
+        """getter width"""
+        return self.__width
 
-@property
-def width(self):
-    """getter width"""
-    return self.__width
+    @width.setter
+    def width(self, value):
+        """setter width"""
+        self.__width = value
 
+    @property
+    def height(self):
+        """getter height"""
+        return self.__height
 
-@width.setter
-def width(self, value):
-    """setter width"""
-    self.__width = value
+    @height.setter
+    def height(self, value):
+        """setter height"""
+        self.__height = value
 
+    @property
+    def x(self):
+        """getter x"""
+        return self.__x
 
-@property
-def height(self):
-    """getter height"""
-    return self.__height
+    @x.setter
+    def x(self, value):
+        """setter x"""
+        self.__x = value
 
+    @property
+    def y(self):
+        """getter y"""
+        return self.__y
 
-@height.setter
-def height(self, value):
-    """setter height"""
-    self.__height = value
-
-
-@property
-def x(self):
-    """getter x"""
-    return self.__x
-
-
-@x.setter
-def x(self, value):
-    """setter x"""
-    self.__x = value
-
-
-@property
-def y(self):
-    """getter y"""
-    return self.__y
-
-
-@y.setter
-def y(self, value):
-    """setter y"""
-    self.__y = value
+    @y.setter
+    def y(self, value):
+        """setter y"""
+        self.__y = value
