acf261c44a8181366b321f92bdddca20bd0af1ac
micaelapicco <micaelaabril421@gmail.com>
2023-06-26 12:39:43 -0700

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,48 +10,56 @@ class Rectangle(Base):
     """Class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__X = x
         self.__y = y
-        super().__init__(id)
 
 
 @property
 def width(self):
+    """getter width"""
     return self.__width
 
 
 @width.setter
-def width(self):
-    self.__width = width
+def width(self, value):
+    """setter width"""
+    self.__width = value
 
 
 @property
 def height(self):
+    """getter height"""
     return self.__height
 
 
 @height.setter
-def height(self):
-    self.__height = height
+def height(self, value):
+    """setter height"""
+    self.__height = value
 
 
 @property
 def x(self):
+    """getter x"""
     return self.__x
 
 
 @x.setter
-def x(self):
-    self.__x = x
+def x(self, value):
+    """setter x"""
+    self.__x = value
 
 
 @property
 def y(self):
+    """getter y"""
     return self.__y
 
 
 @y.setter
-def y(self):
-    self.__y = y
+def y(self, value):
+    """setter y"""
+    self.__y = value
