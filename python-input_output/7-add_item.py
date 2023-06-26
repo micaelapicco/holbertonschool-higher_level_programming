886a5ebedc17e428f76da31ba3cefa5f45fe8683
micaelapicco <micaelaabril421@gmail.com>
2023-06-26 07:15:20 -0700

    try

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,6 @@ and then save them to a file
 import sys
 from os import path
 
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -17,9 +16,8 @@ file = "add_item.json"
 
 if path.exists(file):
     my_list = load_from_json_file(file) + args[1:]
-
 else:
-    my_list = []
+    my_list = [] + args[1:]
 
 save_to_json_file(my_list, file)
 
