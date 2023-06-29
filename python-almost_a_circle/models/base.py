535f929b599e93d2865f1f5ec08de456b7fd4989
micaelapicco <micaelaabril421@gmail.com>
2023-06-29 14:46:22 -0700

    json string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,6 +21,6 @@ class Base():
     def to_json_string(list_dictionaries):
         """json representation of dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return ([])
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
