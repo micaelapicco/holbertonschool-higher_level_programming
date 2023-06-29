41b8b042bdb3ed3f0627971c07136587ad246b7d
micaelapicco <micaelaabril421@gmail.com>
2023-06-29 07:21:03 -0700

    updated arguments

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,4 +90,14 @@ class Rectangle(Base):
             self.id, self.x, self.y, self.width, self.height))
 
     def update(self, *args):
-        
+        """update arguments in order"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
